5b1ee5e891825d34cbfd156528eb1711e509df4a
NilPatel14 <pnil7877@gmail.com>
2024-08-14 20:30:52 +0530

    Subjects and Courses crud done

--- timeTable/urls.py
+++ timeTable/urls.py
@@ -10,5 +10,20 @@ urlpatterns = [
     path('timetable/create/', views.timetable_create, name='timetable_create'),
     path('timetable/<int:pk>/edit/', views.timetable_edit, name='timetable_edit'),
     path('timetable/<int:pk>/delete/', views.timetable_delete, name='timetable_delete'),
-    path('showtimetable',views.showtimetable,name="showtimetable")
+    path('showtimetable',views.showtimetable,name="showtimetable"),
+    path('ajax/load-subjects/', views.load_subjects, name='ajax_load_subjects'),
+
+    #--------- For Courses -----------#
+    path('course-list/',views.course_list,name="course"),
+    path('courses/create/', views.course_create, name='course_create'),
+    path('courses/<int:pk>/edit/', views.course_edit, name='course_edit'),
+    path('courses/<int:pk>/delete/', views.course_delete, name='course_delete'),
+    #-----------------------------------------#
+
+    #--------- For Courses -----------#
+    path('subjects/', views.subject_list, name='subject_list'),
+    path('subjects/create/', views.subject_create, name='subject_create'),
+    path('subjects/<int:pk>/edit/', views.subject_edit, name='subject_edit'),
+    path('subjects/<int:pk>/delete/', views.subject_delete, name='subject_delete'),
+    #-----------------------------------------#
 ] 
